9455ebdeb21108c83f5cb737f90fa329d96726e5
gnfigueroa <gabrieln.figueroa@yahoo.com>
2015-10-19 01:53:02 -0300

    logica de problema propuesto.

---
+++ tpintegrador.c
@@ -0,0 +1,14 @@
+/* 
+TP integrador : Ejemplo presentado por el profesor
+Gabriel Nicolas Figueroa
+28 de septiembre del 2014
+*/
+
+#include <stdio.h>
+
+
+int printf(const char*,...);
+int main(void){
+	int _[:>=<%-!.0,};
+	printf("%d%d",sizeof _-sizeof _[0],sizeof(char)+0[_]);
+}
